fa8c4effda9d14f54d1ccae3b20562c062abcafc
ChihHaoChen <chih-hao.chen2@mail.mcgill.ca>
2020-06-03 09:30:48 +0800

    Initial Commit

---
+++ TISensorTagCC2650Client/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TISensorTagCC2650Client
+//
+//  Created by ChihHao on 2020/06/03.
+//  Copyright © 2020 ChihHao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
